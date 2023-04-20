e66d03f07c46c40ea232341e103eae4669e51800
Noah <3221417948@qq.com>
2023-04-20 17:24:38 +0800

    Mybatis基本使用

--- Mybatis/src/test/test01.java
+++ Mybatis/src/test/test01.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class test01 {
     public static void main(String[] args) {
@@ -35,8 +36,44 @@ public class test01 {
                        参数2：我们要为sgL语句中传递的参数
         *
         * */
-        Student s = session.selectOne("test01.getById", "A001");
+        /*Student s = session.selectOne("test01.getById", "A001");
         System.out.println(s);
+        session.close();*/
+
+        //查询学生信息表中的所有的记录
+       /* List<Student> sList= session.selectList("test01.getAll");
+        for (Student s:sList){
+            System.out.println(s);
+        }
+        session.close();*/
+
+
+        /*
+        *  控制台显示update 数据库表中为变化 原因： Mybatis默认情况下是手动提交事务
+        * 需要添加 session.commit()
+        * */
+        //添加操作
+      /*  Student s=new Student();
+        s.setId("A006");
+        s.setName("cxk");
+        s.setAge(23);
+        int count = session.insert("test01.save", s);
+        System.out.println("+++++++++++++++++"+count);
+        session.commit();
+        session.close();*/
+
+        //修改操作
+       /* Student s=new Student();
+        s.setId("A006");
+        s.setName("cxk1");
+        s.setAge(25);
+        int count = session.insert("test01.update", s);
+        session.commit();
+        session.close();*/
+
+        //删除操作
+        session.delete("test01.delete", "A006");
+        session.commit();
         session.close();
 
     }
